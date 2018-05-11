dc29cba0396e6b44a39f8418ee5a188b5350c726
David Rynn <davidrynn@gmail.com>
2018-05-11 18:13:25 -0400

    Initial Commit

---
+++ LayoutSubviewsExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutSubviewsExperiment
+//
+//  Created by Rynn, David on 5/11/18.
+//  Copyright © 2018 Rynn, David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
